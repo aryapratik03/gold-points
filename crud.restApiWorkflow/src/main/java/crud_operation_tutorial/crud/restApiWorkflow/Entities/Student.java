2b39e41ae81fc86d516f1e2f4beb4903c7f88d78
aryapratik03 <162798253+aryapratik03@users.noreply.github.com>
2026-06-21 10:23:09 +0530

    Add files via upload

---
+++ crud.restApiWorkflow/src/main/java/crud_operation_tutorial/crud/restApiWorkflow/Entities/Student.java
@@ -0,0 +1,42 @@
+package crud_operation_tutorial.crud.restApiWorkflow.Entities;
+
+//import jakarta.*;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name="students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String course;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+// Getter Setter
+}
---
+++ crud.restApiWorkflow/src/test/java/crud_operation_tutorial/crud/restApiWorkflow/ApplicationTests.java
@@ -0,0 +1,13 @@
+package crud_operation_tutorial.crud.restApiWorkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
